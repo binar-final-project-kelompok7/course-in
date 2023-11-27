e8859180e4c00c71f0c262afac2bd4e62df33aff
solahkay <solahudinalmalibari@gmail.com>
2023-11-27 19:50:32 +0700

    Initialization Project

---
+++ src/main/java/com/github/k7/coursein/LearnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.github.k7.coursein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurityApplication.class, args);
+	}
+
+}

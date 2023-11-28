233e32c1cac62d138f48c83868bde6ebcdae90b9
solahkay <solahudinalmalibari@gmail.com>
2023-11-28 11:21:35 +0700

    rename: Main Class

--- src/main/java/com/github/k7/coursein/CourseInApplication.java
+++ src/main/java/com/github/k7/coursein/CourseInApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnSpringSecurityApplication {
+public class CourseInApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringSecurityApplication.class, args);
+		SpringApplication.run(CourseInApplication.class, args);
 	}
 
 }

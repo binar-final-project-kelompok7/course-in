034813481417f8a9f1afe452aec9d74a6f5b21f8
solahkay <solahudinalmalibari@gmail.com>
2023-11-29 22:31:20 +0700

    update: CourseService

--- src/main/java/com/github/k7/coursein/service/CourseServiceImpl.java
+++ src/main/java/com/github/k7/coursein/service/CourseServiceImpl.java
@@ -2,14 +2,14 @@ package com.github.k7.coursein.service;
 
 import com.github.k7.coursein.entity.Course;
 import com.github.k7.coursein.model.CourseResponse;
-import com.github.k7.coursein.model.PagingRequest;
 import com.github.k7.coursein.repository.CourseRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,10 +17,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
+@AllArgsConstructor
+@Service
 public class CourseServiceImpl implements CourseService {
 
-    @Autowired
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
     private static final String COURSE_NOT_FOUND_MESSAGE = "Course not found";
 
@@ -42,17 +43,17 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     @Transactional(readOnly = true)
-    public Page<CourseResponse> getAllCourse(PagingRequest request) {
-        log.info("Fetching all available courses. Page: {}, Size: {}", request.getPage(), request.getSize());
+    public Page<CourseResponse> getAllCourse(int page, int size) {
+        log.info("Fetching all available courses. Page: {}, Size: {}", page, size);
 
-        PageRequest pageRequest = PageRequest.of(request.getPage(), request.getSize());
+        PageRequest pageRequest = PageRequest.of(page, size);
         Page<Course> allCoursesPage = courseRepository.findAll(pageRequest);
 
         List<CourseResponse> courseResponses = allCoursesPage.getContent().stream()
             .map(CourseServiceImpl::toCourseResponse)
             .collect(Collectors.toList());
 
-        log.info("Returning {} courses on page {} of size {}", courseResponses.size(), request.getPage(), request.getSize());
+        log.info("Returning {} courses on page {} of size {}", courseResponses.size(), page, size);
 
         return new PageImpl<>(courseResponses, pageRequest, allCoursesPage.getTotalElements());
     }
--- src/test/java/com/github/k7/coursein/service/CourseServiceTest.java
+++ src/test/java/com/github/k7/coursein/service/CourseServiceTest.java
@@ -84,7 +84,7 @@ public class CourseServiceTest {
 
         when(courseRepository.findAll(Mockito.any(PageRequest.class))).thenReturn(mockPage);
 
-        Page<CourseResponse> resultPage = courseService.getAllCourse(new PagingRequest(0, 8));
+        Page<CourseResponse> resultPage = courseService.getAllCourse(0, 8);
         Assertions.assertEquals(mockCourses.size(), resultPage.getContent().size());
 
         Mockito.verify(courseRepository).findAll(PageRequest.of(0, 8));

c4522061bd8f2b2951fcfa1eba57ce6d22b20570
Diffa Azkhani <diffaazkhani1@gmail.com>
2023-12-01 20:05:00 +0700

    update: updateCourse controller

--- src/main/java/com/github/k7/coursein/controller/CourseController.java
+++ src/main/java/com/github/k7/coursein/controller/CourseController.java
@@ -2,17 +2,14 @@ package com.github.k7.coursein.controller;
 
 import com.github.k7.coursein.model.CourseResponse;
 import com.github.k7.coursein.model.PagingResponse;
+import com.github.k7.coursein.model.UpdateCourseRequest;
 import com.github.k7.coursein.model.WebResponse;
 import com.github.k7.coursein.service.CourseService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -53,6 +50,21 @@ public class CourseController {
                 .totalPage(allCourse.getTotalPages())
                 .size(allCourse.getSize())
                 .build())
+            .build();
+    }
+
+    @PatchMapping(
+        path = "{id}",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<CourseResponse> updateCourse(@PathVariable("id") Long id,
+                                                    @RequestBody UpdateCourseRequest updateCourseRequest) {
+        CourseResponse courseResponse = courseService.updateCourse(id, updateCourseRequest);
+        return WebResponse.<CourseResponse>builder()
+            .code(HttpStatus.OK.value())
+            .message(HttpStatus.OK.getReasonPhrase())
+            .data(courseResponse)
             .build();
     }
 

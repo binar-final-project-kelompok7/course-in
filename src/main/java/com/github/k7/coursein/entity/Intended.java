205e5f7d3129f6c0337743f8afc75b07fa8d3bf7
solahkay <solahudinalmalibari@gmail.com>
2023-12-05 17:47:21 +0700

    update: change relationship between Intended and Course

--- src/main/java/com/github/k7/coursein/entity/Intended.java
+++ src/main/java/com/github/k7/coursein/entity/Intended.java
@@ -12,29 +12,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "intended")
-@ToString(exclude = {"courses"})
-@EqualsAndHashCode(exclude = {"courses"})
+@Table(name = "intendeds")
+@ToString(exclude = {"course"})
+@EqualsAndHashCode(exclude = {"course"})
 public class Intended {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false, length = 200)
+    @Column(length = 200)
     private String purpose;
 
-    @ManyToMany(mappedBy = "intendeds")
-    private Set<Course> courses = new HashSet<>();
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "id", nullable = false)
+    private Course course;
 
 }

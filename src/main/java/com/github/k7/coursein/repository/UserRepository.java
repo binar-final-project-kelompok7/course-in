c8e00dfc4a3b72f18cd128a0718cb57101c7da83
Diffa Azkhani <diffaazkhani1@gmail.com>
2023-12-27 20:32:56 +0700

    update: spring mail properties

--- src/main/java/com/github/k7/coursein/repository/UserRepository.java
+++ src/main/java/com/github/k7/coursein/repository/UserRepository.java
@@ -13,8 +13,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
 
-    Optional<User> findByEmail(String email);
-
     Optional<User> findByUsernameOrEmail(String username, String email);
 
     boolean existsByUsernameOrEmail(String username, String email);

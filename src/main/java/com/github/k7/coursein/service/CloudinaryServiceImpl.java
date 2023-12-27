703f223c17096916963f4e7e68f7ae7641c8f9aa
Irvine <irvineringgo90@gmail.com>
2023-12-27 12:44:16 +0700

    Update: Added image quality adjustment after uploading.

--- src/main/java/com/github/k7/coursein/service/CloudinaryServiceImpl.java
+++ src/main/java/com/github/k7/coursein/service/CloudinaryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.github.k7.coursein.service;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.Transformation;
 import com.cloudinary.utils.ObjectUtils;
 import com.github.k7.coursein.model.UploadImageRequest;
 import lombok.AllArgsConstructor;
@@ -8,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.util.Map;
 
 @AllArgsConstructor
 @Service
@@ -19,23 +19,27 @@ public class CloudinaryServiceImpl implements CloudinaryService {
 
     @Override
     public String upload(String username, UploadImageRequest request) throws IOException {
-        log.info("Uploading");
+        log.info("Uploading {}", request.getMultipartFile().getOriginalFilename());
 
-        Map<?, ?> uploadedImage = cloudinary
-            .uploader()
+        cloudinary.uploader()
             .upload(request.getMultipartFile().getBytes(),
                 ObjectUtils.asMap("public_id", username));
 
+        String link = cloudinary.url().transformation(new Transformation<>()
+            .quality("60")
+            .chain()
+            .dpr("auto")
+        ).generate(username);
+
         log.info("finished");
-        return uploadedImage.get("url").toString();
+
+        return link;
     }
 
     @Override
     public void delete(String username) throws IOException {
         log.info("Deleting profile picture of user {}", username);
-
         cloudinary.uploader().destroy(username, ObjectUtils.asMap("resource_type", "image"));
     }
 
-    //TODO: image compression, ...
 }
